9bcc86a9a43e5739ba21a119c556b022a66b3305
MartinKuracka <61198403+MartinKuracka@users.noreply.github.com>
2020-06-07 18:09:35 +0200

    update

--- js/js.js
+++ js/js.js
@@ -1,23 +1,3 @@
-
-// var textWrapper = document.querySelector('.letters');
-// console.log (textWrapper);
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letters'>$&</span>");
-
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.letters',
-//     translateY: ["1.2em", 0],
-//     translateZ: 0,
-//     duration: 550,
-//     delay: (el, i) => 50 * i
-//   }).add({
-//     targets: '.ml6',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
-
 var textWrapper = document.querySelector('.letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letters'>$&</span>");
 
